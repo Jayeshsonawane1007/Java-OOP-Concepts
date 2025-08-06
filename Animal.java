0b5d3616cb6f125c03d4c1c80a3ca66a83da74b1
Jayesh Pratap Sonawane <149810387+Jayeshsonawane1007@users.noreply.github.com>
2025-08-06 11:45:19 +0530

    Animal.java

--- Animal.java
+++ Animal.java
@@ -11,4 +11,12 @@ public class Dog extends Animal {
     public void makeSound() {
         System.out.println("Dog barks");
     }
+    public static void main(String[] args) {
+        
+     Animal animal = new Animal();
+        Animal dog = new Dog();  // Runtime Polymorphism
+        animal.makeSound();  // Output: Animal makes a sound
+        dog.makeSound();     // Output: Dog barks
+
+}
 }
